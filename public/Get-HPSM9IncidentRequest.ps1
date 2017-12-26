ccde8d2743fb120fb90c1629cc3e038c11c85e61
Chendrayan Venkatesan <chendrayan.venkatesan@pwc.com>
2017-12-26 08:06:47 -0500

    Initial Commit

--- public/Get-HPSM9IncidentRequest.ps1
+++ public/Get-HPSM9IncidentRequest.ps1
@@ -32,31 +32,35 @@ function Get-HPSM9IncidentRequest {
             $IncidentModelType = New-Object ($NameSpace + ".IncidentModelType")
             $IncidentModelType.instance = $IncidentInstanceType
             $IncidentModelType.keys = $IncidentKeysType
-            $Incident = New-Object ($NameSpace + ".StringType") 
-            $Incident.Value = $IncidentId
-            $IncidentKeysType.IncidentID = $Incident
-            $RetrieveIncidentRequest = New-Object ($NameSpace + ".RetrieveIncidentRequest")
-            $RetrieveIncidentRequest.model = $IncidentModelType
-            $Result = $WebService.RetrieveIncident($RetrieveIncidentRequest)
-            if ($Result.Status -eq 'SUCCESS') {
-                [pscustomobject]@{
-                    IncidentId      = $Result.model.instance.IncidentID.Value
-                    ContactGUID     = $Result.model.instance.ContactGUID.Value
-                    AssignmentGroup = $Result.model.instance.AssignmentGroup.Value
-                    OpenTime        = $Result.model.instance.OpenTime.Value 
-                    ClosedTime      = $Result.model.instance.ClosedTime.Value
-                    Status          = $Result.model.instance.Status.Value
-                    TimeToClose     = if ($Result.model.instance.Status.Value -eq "Closed") {
-                        $Difference = New-TimeSpan -End $Result.model.instance.ClosedTime.Value -Start $Result.model.instance.OpenTime.Value 
-                        "{0}:{1}:{2}:{3}" -f ($Difference.Days, $Difference.Hours, $Difference.Minutes, $Difference.Seconds)
+            $Incident = New-Object ($NameSpace + ".StringType")
+            foreach ($Incidents in $IncidentId) {
+
+                $Incident.Value = $Incidents
+                $IncidentKeysType.IncidentID = $Incident
+                $RetrieveIncidentRequest = New-Object ($NameSpace + ".RetrieveIncidentRequest")
+                $RetrieveIncidentRequest.model = $IncidentModelType
+                $Result = $WebService.RetrieveIncident($RetrieveIncidentRequest)
+                if ($Result.Status -eq 'SUCCESS') {
+                    [pscustomobject]@{
+                        IncidentId      = $Result.model.instance.IncidentID.Value
+                        ContactGUID     = $Result.model.instance.ContactGUID.Value
+                        AssignmentGroup = $Result.model.instance.AssignmentGroup.Value
+                        OpenTime        = $Result.model.instance.OpenTime.Value 
+                        ClosedTime      = $Result.model.instance.ClosedTime.Value
+                        Status          = $Result.model.instance.Status.Value
+                        TimeToClose     = if ($Result.model.instance.Status.Value -eq "Closed") {
+                            $Difference = New-TimeSpan -End $Result.model.instance.ClosedTime.Value -Start $Result.model.instance.OpenTime.Value 
+                            "{0}:{1}:{2}:{3}" -f ($Difference.Days, $Difference.Hours, $Difference.Minutes, $Difference.Seconds)
+                        }
                     }
                 }
+                else {
+                    $Message = "{0} with the message {1} for {2}" -f ($Result.Status, $Result.message , $Incidents)
+                    Write-Warning -Message $Message
+                }
+                $WebService.Dispose()
             }
-            else {
-                $Message = "{0} with the message {1}" -f ($Result.Status, $Result.message)
-                Write-Warning -Message $Message
-            }
-            $WebService.Dispose()
+            
         }
         catch {
             $_.Exception.ErrorRecord 

ba1c1ba08a5e441ae09716b506e190ca669b7a44
Chendrayan Venkatesan <chendrayan.venkatesan@pwc.com>
2017-12-26 06:48:59 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ A repository for sharing PowerShell codes to automate HPSM9
 - [x] `Get-HPSM9IncidentRequest`
 - [x] `Get-HPSM9ChangeRequest`
 - [ ] `Get-HPSM9ProblemRequest`
-- [ ] `Get-HPSM9DeviceInformation`
+- [x] `Get-HPSM9DeviceInformation`
 - [ ] `Register-HPSM9IncidentRequest`
 - [ ] `Approve-HPSM9ChangeRequest`
 - [ ] `Close-HPSM9IncidentRequest`
 
+> We are working on the web service proxy object to find the methods dynamically before invoking it! 
+
 ### Disclaimer
-This module is built on the fly based on the request from the Incident, Change, Problem and Configuration management team. All the shared scripts are packaged as module. So, possible mistakes are expected. 
+This module is built on the fly based on the request from the Incident, Change, Problem and Configuration management team. All the shared scripts are packaged as module. So, possible mistakes are expected.
--- public/Get-HPSM9DeviceInformation.ps1
+++ public/Get-HPSM9DeviceInformation.ps1
@@ -1,15 +1,66 @@
 function Get-HPSM9DeviceInformation {
     [CmdletBinding()]
     param (
-        
-    )
+        [Parameter(Mandatory)]
+        $Uri,
     
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]]
+        $ConfigurationItem,
+    
+        [Parameter()]
+        [System.Management.Automation.CredentialAttribute()]
+        [pscredential]
+        $Credential
+    )
+        
     begin {
     }
-    
+        
     process {
+        try {
+            $WebService = New-WebServiceProxy -uri $Uri -ErrorAction Stop
+            if ($PSBoundParameters.ContainsKey('Credential')) {
+                $WebService.Credentials = [System.Net.NetworkCredential]::new($Credential.UserName, $Credential.Password)
+            }
+            else {
+                $WebService.UseDefaultCredentials = $true
+            }
+            $NameSpace = $WebService.GetType().NameSpace
+            $DeviceKeysType = New-Object ($NameSpace + ".DeviceKeysType")
+            $DeviceInstanceType = New-Object ($NameSpace + ".DeviceInstanceType")
+            $DeviceModelType = New-Object ($NameSpace + ".DeviceModelType")
+            $DeviceModelType.instance = $DeviceInstanceType
+            $DeviceModelType.keys = $DeviceKeysType
+
+            $CIs = New-Object ($NameSpace + ".StringType")
+            foreach ($CI in $ConfigurationItem) {
+                $CIs.Value = $CI
+                $DeviceInstanceType.ConfigurationItem = $CIs
+                $RetrieveDeviceRequest = New-Object ($namespace + ".RetrieveDeviceRequest")
+                $RetrieveDeviceRequest.model = $DeviceModelType
+
+                $Result = $WebService.RetrieveDevice($RetrieveDeviceRequest)
+                if ($Result.status -eq "SUCCESS") {
+                    [pscustomobject]@{
+                        ConfigurationItem = $Result.model.instance.recordid
+                        AssignmentGroup   = $Result.model.instance.AssignmentGroup.value 
+                        Environment       = $Result.model.instance.Environment.Value
+                        Location          = $Result.model.instance.Location.Value
+                    }
+                }
+                else {
+                    $Message = "{0} with the message {1} for {2}" -f ($Result.Status, $Result.message , $CI)
+                    Write-Warning -Message $Message
+                }
+                $WebService.Dispose()
+            }
+        }
+        catch {
+            $_.Exception.ErrorRecord 
+        }
     }
-    
+        
     end {
     }
 }
